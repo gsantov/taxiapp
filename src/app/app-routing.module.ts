ebaf8d82ddead8fddf2b9f2aa0044af89ad035de
genarosulca@hotmail.com <genarosulca@hotmail.com>
2022-03-22 18:00:48 -0500

    routing optim

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,6 @@ import { LayoutComponent } from './components/layout.component';
 import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
   {
     path: '',
